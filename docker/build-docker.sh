9a4990e4563f944f5ecd8d5781dea087a7b19281
Leonardo Belissimo <muto.leonardo@hcosta.com.br>
2025-05-15 09:23:35 -0300

    Ajustes no script de build e dockerfile. Corrigido problema de permissão nos volumes, adicionando keys importantes

--- docker/build-docker.sh
+++ docker/build-docker.sh
@@ -13,3 +13,15 @@ echo "UID=$(id -u)" >> .env
 echo "GID=$(id -g)" >> .env
 
 docker compose up -d --build
+
+if [ ! -d "vendor" ]; then
+  docker compose exec app composer install --no-dev --optimize-autoloader
+fi
+
+if ! grep -q '^APP_KEY=base64:' .env; then
+  docker compose exec app php artisan key:generate
+fi
+
+if grep -q '^JWT_SECRET=' .env && ! grep -q '^JWT_SECRET=base64:' .env; then
+  docker compose exec app php artisan jwt:secret
+fi
